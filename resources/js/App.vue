c84de12ae1fcd92f5c8bf3e2934c7bc26f8d2893
Kengketa <kengketa@gmail.com>
2023-02-01 11:58:09 +0700

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg-red-500 min-h-screen flex justify-center items-center text-xl text-white">
+        {{data}}
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        data:{
+            type:String,
+            default:null
+        }
+    },
+    data() {
+        return {
+            message: 'Hello World!',
+        }
+    }
+}
+</script>
